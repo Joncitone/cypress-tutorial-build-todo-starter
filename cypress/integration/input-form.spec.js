7763549759dcd0a68f8c82778243b68b46b61020
Joncitone <jarreola7123@gmail.com>
2021-03-21 18:39:59 -0500

    finished step 2, text inputs

--- cypress/integration/input-form.spec.js
+++ cypress/integration/input-form.spec.js
@@ -1,5 +1,12 @@
 describe('Input form', () => {
+  beforeEach(() => {
+    cy.visit('/');
+  });
   it('focuses input on load', () => {
-    cy.visit('http://localhost:3030');
+    cy.focused().should('have.class', 'new-todo');
+  });
+  it('accepts input', () => {
+    const typedText = 'Practice Cypress';
+    cy.get('.new-todo').type(typedText).should('have.value', typedText);
   });
 });
