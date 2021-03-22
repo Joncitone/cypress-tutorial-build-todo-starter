e728adc76548dd2854f721b5c558f7e3a57a4cf1
Joncitone <jarreola7123@gmail.com>
2021-03-22 00:53:44 -0500

    finished part 8, end-to-end part 1

---
+++ cypress/integration/smoke-tests.spec.js
@@ -0,0 +1,27 @@
+describe('Smoke tests', () => {
+  beforeEach(() => {
+    cy.request('GET', '/api/todos')
+      .its('body')
+      .each((todo) => cy.request('DELETE', `/api/todos/${todo.id}`));
+  });
+  context('with no todos', () => {
+    it.only('Saves new todos', () => {
+      const items = [
+        { text: 'Buy Milk', expectedLength: 1 },
+        { text: 'Buy Eggs', expectedLength: 2 },
+        { text: 'Buy Bread', expectedLength: 3 },
+      ];
+      cy.visit('/');
+      cy.server();
+      cy.route('POST', '/api/todos').as('create');
+
+      cy.wrap(items).each((todo) => {
+        cy.focused().type(todo.text).type('{enter}');
+
+        cy.wait('@create');
+
+        cy.get('.todo-list li').should('have.length', todo.expectedLength);
+      });
+    });
+  });
+});
