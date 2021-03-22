9b0e4e717bc024b4d9f3de93cff11a085d5cca7d
Joncitone <jarreola7123@gmail.com>
2021-03-21 19:15:03 -0500

    finished step 3, form submission and stubbed API calls

--- cypress/integration/input-form.spec.js
+++ cypress/integration/input-form.spec.js
@@ -9,4 +9,36 @@ describe('Input form', () => {
     const typedText = 'Practice Cypress';
     cy.get('.new-todo').type(typedText).should('have.value', typedText);
   });
+
+  context('Form submission', () => {
+    beforeEach(() => {
+      cy.server();
+    });
+    it('Adds a new todo when submitted', () => {
+      const itemText = 'Buy chips';
+      cy.route('POST', '/api/todos', {
+        name: itemText,
+        id: 1,
+        isComplete: false,
+      });
+
+      cy.get('.new-todo')
+        .type(itemText)
+        .type('{enter}')
+        .should('have.value', '');
+
+      cy.get('.todo-list li').should('have.length', 1).and('contain', itemText);
+    });
+    it('Shows an error message on a failed submission', () => {
+      cy.route({
+        url: '/api/todos',
+        method: 'POST',
+        status: 500,
+        response: {},
+      });
+      cy.get('.new-todo').type('test{enter}');
+      cy.get('.todo-list li').should('not.exist');
+      cy.get('.error').should('be.visible');
+    });
+  });
 });
